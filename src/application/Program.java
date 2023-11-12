c7826e86342be9777a0310e43e76212f5841ac39
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2023-11-12 16:07:43 -0300

    feat: Piece and Board classes created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
+import board_game.Board;
+import board_game.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Board board = new Board(8,8);
 	}
 
 }
