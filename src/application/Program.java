e337948f42e1befb914ddbdc01035d109f72dd98
Luiz Felipe de Souza Barbosa <luizfelipesouza1998@outlook.com>
2023-11-12 16:42:42 -0300

    feat: create chess layer and class to print the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,14 @@
 package application;
-import board_game.Board;
-import board_game.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Board board = new Board(8,8);
+		ChessMatch chessMatch = new ChessMatch();
+		
+		UI.printBoard(chessMatch.getPieces());
 	}
 
 }
+	
